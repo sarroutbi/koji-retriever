48c25540584ba5451c4ffd4f756e9ef3e5d96491
Sergio Arroutbi <sarroutb@redhat.com>
2023-03-09 11:36:33 +0100

    Include koji-retriever command line test (#15)
    Resolves: #14
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ tests/koji-retriever-test.rs
@@ -0,0 +1,21 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn url_existing_test() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("koji-retriever")?;
+    cmd.arg("-u").arg("https://koji.fedoraproject.org/koji/buildinfo?buildID=2166955")
+        .arg("-d").arg("/tmp");
+    cmd.assert().success()
+        .stdout(predicate::str::contains("/tmp/pykickstart-3.45-1.fc39.src.rpm"));
+    Ok(())
+}
+
+#[test]
+fn url_does_not_exist_test() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("koji-retriever")?;
+    cmd.arg("-u").arg("https://unexisting-url.almost.sure");
+    cmd.assert().failure();
+    Ok(())
+}
