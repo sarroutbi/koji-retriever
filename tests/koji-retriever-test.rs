08fa47c32931b00a64b1fce0abee5d3490cf5439
Sergio Arroutbi <sarroutb@redhat.com>
2023-03-10 09:30:32 +0100

    Fix format in tests by using rustfmt
    Resolves: #16
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tests/koji-retriever-test.rs
+++ tests/koji-retriever-test.rs
@@ -3,17 +3,20 @@ use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
 #[test]
-fn url_existing_test() -> Result<(), Box<dyn std::error::Error>>{
+fn url_existing_test() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("koji-retriever")?;
-    cmd.arg("-u").arg("https://koji.fedoraproject.org/koji/buildinfo?buildID=2166955")
-        .arg("-d").arg("/tmp");
-    cmd.assert().success()
-        .stdout(predicate::str::contains("/tmp/pykickstart-3.45-1.fc39.src.rpm"));
+    cmd.arg("-u")
+        .arg("https://koji.fedoraproject.org/koji/buildinfo?buildID=2166955")
+        .arg("-d")
+        .arg("/tmp");
+    cmd.assert().success().stdout(predicate::str::contains(
+        "/tmp/pykickstart-3.45-1.fc39.src.rpm",
+    ));
     Ok(())
 }
 
 #[test]
-fn url_does_not_exist_test() -> Result<(), Box<dyn std::error::Error>>{
+fn url_does_not_exist_test() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("koji-retriever")?;
     cmd.arg("-u").arg("https://unexisting-url.almost.sure");
     cmd.assert().failure();
