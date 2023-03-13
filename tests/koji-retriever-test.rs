6333f3935032c98b9adc1385f85bffbbba7b265e
Sergio Arroutbi <sarroutb@redhat.com>
2023-03-13 10:20:51 +0100

    Use global variable for koji-retriever binary
    Resolves: #27
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tests/koji-retriever-test.rs
+++ tests/koji-retriever-test.rs
@@ -2,9 +2,11 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
+const KOJI_RETRIEVER_BINARY: &str = "koji-retriever";
+
 #[test]
 fn url_existing_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("koji-retriever")?;
+    let mut cmd = Command::cargo_bin(KOJI_RETRIEVER_BINARY)?;
     cmd.arg("-u")
         .arg("https://koji.fedoraproject.org/koji/buildinfo?buildID=2166955")
         .arg("-d")
@@ -17,7 +19,7 @@ fn url_existing_test() -> Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn url_does_not_exist_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("koji-retriever")?;
+    let mut cmd = Command::cargo_bin(KOJI_RETRIEVER_BINARY)?;
     cmd.arg("-u").arg("https://unexisting-url.almost.sure");
     cmd.assert().failure();
     Ok(())
@@ -25,7 +27,7 @@ fn url_does_not_exist_test() -> Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn url_existing_verbose_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("koji-retriever")?;
+    let mut cmd = Command::cargo_bin(KOJI_RETRIEVER_BINARY)?;
     cmd.arg("-u")
         .arg("https://koji.fedoraproject.org/koji/buildinfo?buildID=2166955")
         .arg("-v")
